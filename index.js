ca53c8e114d53b0f87039a1e75c248c89604ca33
Rub21 <rub2106@gmail.com>
2019-02-04 12:35:19 -0500

    Filter data

---
+++ index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const argv = require('minimist')(process.argv.slice(2));
+const tileReduce = require('@mapbox/tile-reduce');
+const path = require('path');
+
+tileReduce({
+  bbox: argv.bbox,
+  zoom: 15,
+  map: path.join(__dirname, '/map.js'),
+  sources: [
+    // {
+    //   name: 'pop',
+    //   mbtiles: argv._[0],
+    //   raw: false
+    // },
+    {
+      name: 'ml',
+      mbtiles: argv._[0],
+      raw: false
+    },
+  ]
+})
+  .on('reduce', function () { })
+  .on('end', function () { });
---
+++ map.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const turf = require('@turf/turf');
+const _ = require('underscore');
+var rbush = require('rbush');
+
+module.exports = function (tileLayers, tile, writeData, done) {
+  let mlBboxes = [];
+  let popBboxes = [];
+  const mlLayer = tileLayers.ml.ml;
+  // const popLayer = tileLayers.pop.pop;
+  let mlFeatures = {}
+  /** 
+   * Set the variables here
+    */
+  const threshold = 0.92;
+
+  for (let i = 0; i < mlLayer.features.length; i++) {
+    const mlFeature = mlLayer.features[i];
+    if (mlFeature.properties.p1 >= threshold) {
+      const mlId = `m${i}`;
+      mlFeatures[mlId] = mlFeature;
+      // mlBboxes.push(objBbox(mlFeature, mlId));
+    }
+  }
+
+
+  // writeData(mlBboxes.length + '\n')
+  // writeData(JSON.stringify(mlLayer) + '\n');
+  writeData(JSON.stringify(turf.featureCollection(_.values(mlFeatures))) + '\n');
+
+
+  // for (let d = 0; d < populationLayer.features.length; d++) {
+  //   const popFeature = populationLayer.features[d];
+  //   const popId = `p${d}`;
+  //   popBboxes.push(objBbox(popFeature, popId));
+  // }
+
+  // const bboxes = [].concat(mlBboxes).concat(popBboxes);
+  // const tree = rbush(bboxes.length);
+  // tree.load(bboxes);
+  // let results = {}
+
+  // for (let z = 0; z < popBboxes.length; z++) {
+  //   const bbox = popBboxes[z];
+  //   var overlaps = tree.search(bbox);
+  //   writeData(JSON.stringify(overlaps) + '\n')
+
+  //   for (var k = 0; k < overlaps.length; k++) {
+  //     const overlap = overlaps[k];
+  //     // writeData(JSON.stringify(overlap) + '\n')
+  //     // if (bbox.id !== overlap.id && overlap.id.charAt(0) === 'm') {
+  //     //   results[overlap.id] = mlFeatures[overlap.id];
+  //     // }
+  //   }
+  // }
+  // const results = _.values(mlFeatures);
+  // if (results.length > 0) {
+  // writeData(JSON.stringify(turf.featureCollection(results)) + '\n');
+  // writeData(JSON.stringify(popLayer) + '\n');
+
+  // }
+  done(null, null);
+};
+
+function objBbox(obj, id) {
+  const bboxExtent = ['minX', 'minY', 'maxX', 'maxY'];
+  let bbox = { id };
+  const centroid = turf.centroid(obj);
+  // console.log(centroid.geometry.coordiantes)
+  const valBbox = turf.bbox(turf.centroid(obj));
+
+  for (var d = 0; d < valBbox.length; d++) {
+    bbox[bboxExtent[d]] = valBbox[d];
+  }
+  return bbox;
+}
